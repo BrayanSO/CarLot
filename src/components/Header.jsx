b87f1e8a053adf084cf0ed14787e50ac31a829f0
BrayanSO <briansolera@gmail.com>
2023-09-08 02:01:01 +0000

    icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,8 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import CarLogo from "../logo/CarLogo.png"
+import "../Styles/Header.css"
 
 function Header() {
   return (
@@ -11,7 +13,7 @@ function Header() {
         <Container>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link as={Link}to="/">Carlot</Nav.Link>
+              <Nav.Link as={Link}to="/"><img src={CarLogo} alt="Logo"></img></Nav.Link>
               {/* Usa el componente Link para redireccionar a Register */}
               <Nav.Link as={Link} to="/LoginForm">Login</Nav.Link>
             </Nav>
