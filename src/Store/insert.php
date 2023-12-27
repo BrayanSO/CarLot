9285b32938044af39420295fc64f81a78c333a7c
Brayan Solera (BlackHeart) <briansolera@gmail.com>
2023-12-26 19:33:01 -0600

    resureccion

--- src/Store/insert.php
+++ src/Store/insert.php
@@ -1,5 +1,5 @@
 <?php
-// Establece la conexión con la base de datos MySQL
+/* Establece la conexión con la base de datos MySQL
 $servername = "nombre_del_servidor";
 $username = "nombre_de_usuario";
 $password = "contraseña";
@@ -12,19 +12,22 @@ if ($conn->connect_error) {
 }
 
 // Obtén los datos enviados desde la aplicación React (pueden ser a través de POST o GET, según tu necesidad)
-$brandName = $_POST['brand'];
-$modelName = $_POST['model'];
-$style = $_POST['style'];
-$transmission = $_POST['transmission'];
-$price = $_POST['price'];
-$fuel = $_POST['fuel'];
-$doors = $_POST['doors'];
-$kilometres = $_POST['kilometres'];
-$images = json_encode($_POST['images']); // Asumiendo que las imágenes se envían como un arreglo
+$json = file_get_contents('php://input');
+$data = json_decode($json, true);
+
+$brand_id = $data['brand'];
+$model_id = $data['model'];
+$style = $data['style'];
+$transmission = $data['transmission'];
+$price = $data['price'];
+$fuel = $data['fuel'];
+$doors = $data['doors'];
+$kilometres = $data['kilometres'];
+$images = json_encode($data['images']); // Asumiendo que las imágenes se envían como un arreglo
 
 // Consulta preparada para obtener la ID de la marca
 $brandQuery = $conn->prepare("SELECT id FROM brands WHERE name = ?");
-$brandQuery->bind_param("s", $brandName);
+$brandQuery->bind_param("s", $brand_id);
 $brandQuery->execute();
 $brandResult = $brandQuery->get_result();
 $brandRow = $brandResult->fetch_assoc();
@@ -32,17 +35,17 @@ $brandId = $brandRow['id'];
 
 // Consulta preparada para obtener la ID del modelo
 $modelQuery = $conn->prepare("SELECT id FROM models WHERE name = ?");
-$modelQuery->bind_param("s", $modelName);
+$modelQuery->bind_param("s", $model_id);
 $modelQuery->execute();
 $modelResult = $modelQuery->get_result();
 $modelRow = $modelResult->fetch_assoc();
 $modelId = $modelRow['id'];
 
 // Realiza la inserción en la base de datos
-$sql = $conn->prepare("INSERT INTO autos (marca_id, modelo_id, estilo, transmision, precio, combustible, puertas, kilometraje, imagenes) 
+$sql = $conn->prepare("INSERT INTO autos (brand_id, model_id, estilo, transmision, precio, combustible, puertas, kilometraje, imagenes) 
                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
-$sql->bind_param("iisssssss", $brandId, $modelId, $style, $transmission, $price, $fuel, $doors, $kilometres, $images);
+$sql->bind_param("iisssssss", $brand_id, $model_id, $style, $transmission, $price, $fuel, $doors, $kilometres, $images);
 
 if ($sql->execute()) {
     echo "Datos insertados correctamente";
